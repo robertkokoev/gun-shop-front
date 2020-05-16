bbb26317ac790fb1489a84225898bd5daa346f2d
robertkokoev <kokoev.robert@mail.ru>
2020-05-16 13:53:34 +0300

    Добавил фоновую картинку. Вынес в компонент текст с border-bottom, который используется для наименований страинц.

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CardsListComponent } from './cards-list/cards-list.component';
 import { CommonModule } from '@angular/common';
 import { NzButtonModule, NzIconModule } from 'ng-zorro-antd';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [CardsListComponent],
+  declarations: [CardsListComponent, TitleComponent],
   imports: [
     CommonModule,
     NzButtonModule,
     NzIconModule
   ],
-  exports: [CardsListComponent]
+  exports: [CardsListComponent, TitleComponent]
 })
 export class SharedModule { }
